1d93a69421995c5ad870f473ff3cee9f6b534170
Remilia-Scarlet <russel@sgfc.co>
2015-06-07 22:20:31 +0800

    Add all files.

--- README.md
+++ README.md
@@ -9,13 +9,16 @@ Serves as a replacement for a BETV server. (default: pclist.zzfdi.com)
 
 2.  Grab the latest channel XML files for BETV. (I have provided the required XML files already, this is if you want to update them yourself)
 
-    1.  Get Uuid from: http://pclist.zzfdi.com/opsbetv/auth.action?type=1&ack=0000007400010000000000000075
+    1.  Get Uuid from:
+
+        > http://pclist.zzfdi.com/opsbetv/auth.action?type=1&ack=0000007400010000000000000075
+
     2.  Use anything to download the XML from the following URL to `chn_response.xml`. A curl example follows.
-        
+
         ```bash
 curl "http://pclist.yyjbg.com/opsbetv/tv.action?type=1&uuid=<Uuid here>" -H "User-Agent: BETV" -o chn_response.xml
         ```
-    
+
 3. Grab the lastest channel XML files for NIJI. (Read #2 note)
 
     Use anything to download the XML from the following URL to `jp_response.xml`
@@ -24,11 +27,11 @@ curl "http://nebox.myniji.tv:7006/xml/tv1/2/tv.xml" -o jp_response.xml
     ```
 
 4. Install python dependencies.
-    
+
     ```bash
-pip install xmltodict
+pip install xmltodict bottle requests
     ```
-    
+
 5. Modify your `/etc/hosts` file. (`C:\Windows\System32\drivers\etc\hosts` on Windows)
 
     ```
---
+++ betv.py
@@ -0,0 +1,153 @@
+﻿# This Python file uses the following encoding: utf-8
+# BETV Proxy for japanese tv viewing.
+from bottle import get, request, template, run
+import requests
+import xmltodict
+import collections
+import operator
+
+korean_channels = {
+    u"CCTV5+体育赛事": "OCN",
+    u"CCTV5体育频道": "JTBC",
+    u"STAR SPORTS": "",
+    u"CCTV风云足球": "KBS drama HD",
+    u"欧洲足球": "MBC",
+    u"劲爆体育": "",
+    u"高尔夫网球": "KBS 1",
+    u"CCTV13新闻频道": "KBS 2",
+    u"鳳凰衛視資訊臺": "SBS",
+    u"鳳凰衛視中文臺": "YTN",
+    u"CNN": "",
+    u"BBC": "KBS N SPORTS",
+    u"浙江卫视高清": "TV N",
+    u"CCTV1综合频道": "SBS Plus",
+    u"湖南卫视高清": "EBS 1",
+    u"江苏卫视高清": "MBC Drama",
+    u"CCTV3综艺频道": "",
+    u"东方卫视高清": "",
+    u"北京卫视高清": "MBC",
+    u"广东卫视高清": "",
+    u"黑龙江卫视高清": "SBS Golf",
+    u"CCTV11戏曲频道": "J Golf HD",
+    u"浙江卫视高清": "",
+    u"CCTV1综合频道": "SBS Plus",
+    u"湖南卫视高清": "",
+    u"CCTV6电影频道": "",
+    u"HBO": "",
+    u"CCTV4中文国际频道": "TV N",
+    u"國家地理": "",
+    u"探索頻道": "",
+    u"CCTV10科教频道": "MBS SPORTS",
+    u"CCTV12社会与法频道": "SBS Sports",
+    u"CCTV2财经频道": "KBS2",
+    u"第一财经": "KBS N SPORTS",
+    u"CCTV14少儿频道": "KBS1"
+}
+
+xmldoc_jp = xmltodict.parse(open('jp_response.xml'))
+xmldoc_cn = xmltodict.parse(open('chn_response.xml'))
+
+jp = xmldoc_jp["groups"]["group"]["channel"]
+chn_doc = [y for y in xmldoc_cn["channels"]["group"]]
+chn = []
+reduce(operator.iadd, (x["channel"]
+       for x in chn_doc if "channel" in x), chn)
+
+dict_xml = {
+    "channels": {
+        "@version": "",
+        "@info_time": "",
+        "@adsurl": "",
+        "group": [
+            {
+                "@id": "1",
+                "@type": "1",
+                "@name": "Japanese",
+                "@description": "Japanese",
+                "@deploy": "0",
+                "channel": []
+            },
+            {
+                "@id": "2",
+                "@type": "1",
+                "@name": "Korean",
+                "@description": "Korean",
+                "@deploy": "0",
+                "channel": []
+            }
+        ]
+    }
+}
+
+
+for idx, channel in enumerate(jp):
+    formed_channel = collections.OrderedDict((
+        ("name", channel["name"]),
+        ("id", idx + 1),
+        ("status", "1"),
+        ("vip", "0"),
+        ("sop_address", {
+            "item": channel["sop_address"]["item"]
+        }),
+        ("description", ""),
+        ("logo", "")
+    ))
+    dict_xml["channels"]["group"][0]["channel"].append(formed_channel)
+
+for idx, channel in enumerate(chn):
+    if not isinstance(channel, unicode):
+        if korean_channels[channel["name"]] != "":
+            name = channel["name"]
+            channel["name"] = u"{} ({})".format(korean_channels[name], name)
+            dict_xml["channels"]["group"][1]["channel"].append(channel)
+
+
+def replace_url(request):
+    headers = {}
+    for x in dict(request.headers):
+        if x != "Content-Length":
+            headers[x] = dict(request.headers)[x].replace(
+                'zzfdi.com', 'yyjbg.com')
+
+    url = request.url.replace('zzfdi.com', 'yyjbg.com')
+
+    return {"headers": headers, "url": url}
+
+
+@get('/opsbetv/auth.action')
+def auth_handler():
+    info = replace_url(request)
+
+    r = requests.get(
+        info['url'], headers=info['headers'])
+
+    return r.text
+
+
+@get('/opsbetv/tv.action')
+def channel_list():
+    info = replace_url(request)
+
+    return xmltodict.unparse(dict_xml)
+
+
+@get('/opsbetv/tvitem.action')
+def channel_list1():
+    info = replace_url(request)
+
+    r = requests.get(
+        info['url'], headers=info['headers'])
+
+    return r.text
+
+
+@get('/opsbetv/pcad.action')
+def channel_list1():
+    info = replace_url(request)
+
+    r = requests.get(
+        info['url'], headers=info['headers'])
+
+    return r.text
+
+run(host='127.0.0.1', port=80, debug=True)
